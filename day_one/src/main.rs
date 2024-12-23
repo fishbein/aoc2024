7920772804270ae618a118210a6e89223bc6033f
Phil Fishbein <phil@fishbe.in>
2024-12-23 14:50:29 -0600

    smarter rust choices

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,9 +1,7 @@
-use std::fs;
 fn main() {
-    let raw_input = fs::read_to_string("/Users/phil/Developer/aoc2024/day_one/src/input.txt")
-        .expect("Should have been able to read the file");
+    let input = include_str!("./input.txt");
 
-    let [list_one, list_two] = parse_and_sort_lists(&raw_input);
+    let (list_one, list_two) = parse_and_sort_lists(input);
 
     let mut total_distance: i32 = 0;
 
@@ -13,32 +11,34 @@ fn main() {
 
     println!("{total_distance}");
 
-    let mut total_similarity_scores: i32 = 0;
+    let mut total_similarity_scores: i32 = list_one
+        .iter()
+        .map(|n| get_similarity_score(*n, &list_two))
+        .sum::<i32>();
+
     for list_value in list_one {
         total_similarity_scores += get_similarity_score(list_value, &list_two)
     }
     println!("{total_similarity_scores}")
 }
 
-fn parse_and_sort_lists(list: &str) -> [Vec<i32>; 2] {
+fn parse_and_sort_lists(list: &str) -> (Vec<i32>, Vec<i32>) {
     let pairs = list.lines();
 
     let mut list_one: Vec<i32> = Vec::new();
     let mut list_two: Vec<i32> = Vec::new();
 
     for line in pairs {
-        let mut split_line = line.split_whitespace();
-        let num_one = split_line.next().expect("No num one!");
-        let num_two = split_line.next().expect("No num two!");
+        let (num_one, num_two) = line.split_once("   ").expect("Failed to parse line");
 
         list_one.push(num_one.parse().expect("Failed to parse string"));
         list_two.push(num_two.parse().expect("Failed to parse string"));
     }
 
-    list_one.sort();
-    list_two.sort();
+    list_one.sort_unstable();
+    list_two.sort_unstable();
 
-    [list_one, list_two]
+    (list_one, list_two)
 }
 
 fn get_similarity_score(value: i32, compare_list: &[i32]) -> i32 {
